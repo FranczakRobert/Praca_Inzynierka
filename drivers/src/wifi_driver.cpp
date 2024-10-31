f1bbe18f25857fa9359283ecd2cfd4965a7ef0d1
RobertFranczak <Franczak789@gmail.com>
2024-10-31 17:13:03 +0100

    Wifi Connecting

--- drivers/src/wifi_driver.cpp
+++ drivers/src/wifi_driver.cpp
@@ -1,7 +1,10 @@
 #include "wifi_driver.hpp"
 #include "driver_abstract.hpp"
+#include "wifi_config.hpp"
 
 const char* TAG = "WifiDriver";
+
+#define EXAMPLE_ESP_MAXIMUM_RETRY  5
 
 
 int retry_num=0;
@@ -13,45 +16,61 @@ WifiDriver::WifiDriver() {
 
 WifiDriver::~WifiDriver() {
     deinit();
+    ESP_ERROR_CHECK(nvs_flash_erase());
+
 }
 
 int WifiDriver::init() {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-    return 0;
-}
-
-int WifiDriver::deinit() {
-    ESP_LOGI(TAG, "Initialized successfully");
-    return 0;
-}
-
-int WifiDriver::start() {
-    nvs_flash_init(); // this is important in wifi case to store configurations , code will not work if this is not added
-    wifi_connection();
-    return 0;
-}
-
-void WifiDriver::wifi_connection() {
-    esp_netif_init(); //network interdace initialization
-    esp_event_loop_create_default(); //responsible for handling and dispatching events
+    ESP_ERROR_CHECK(esp_netif_init()); //network interdace initialization
+    ESP_ERROR_CHECK(esp_event_loop_create_default()); //responsible for handling and dispatching events
     esp_netif_create_default_wifi_sta(); //sets up necessary data structs for wifi station interface
 
     wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();//sets up wifi wifi_init_config struct with default values
     esp_wifi_init(&wifi_initiation); //wifi initialised with dafault wifi_initiation
 
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);//creating event handler register for wifi
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);//creating event handler register for ip event
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT,
+                                               ESP_EVENT_ANY_ID,
+                                               &WifiDriver::wifi_event_handler,
+                                               NULL));
 
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT,
+                                               IP_EVENT_STA_GOT_IP,
+                                               &WifiDriver::wifi_event_handler,
+                                               NULL));
+    return 0;
+}
 
+int WifiDriver::deinit() {
+    ESP_LOGI(TAG, "Deinicialized successfully");
+    return 0;
+}
+
+int WifiDriver::start() {
+    wifi_connection();
+    return 0;
+}
+
+void WifiDriver::wifi_connection() {
     wifi_config_t wifi_configuration;
+    memset(&wifi_configuration, 0, sizeof(wifi_config_t));
     strcpy((char*)wifi_configuration.sta.ssid, NAME);
-    strcpy((char*)wifi_configuration.sta.password, PSSWD);   
+    strcpy((char*)wifi_configuration.sta.password, PSSWD);
+    wifi_configuration.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
 
-    esp_wifi_set_config(WIFI_IF_STA, &wifi_configuration);  // ustawienie konfiguracji dla stacji (klienta)
-    esp_wifi_start();//start connection with configurations provided in funtion
-    esp_wifi_set_mode(WIFI_MODE_STA);//station mode selected
-    esp_wifi_connect(); //connect with saved ssid and pass
-    printf( "wifi_init_softap finished. SSID:%s  password:%s",wifi_configuration.sta.ssid,wifi_configuration.sta.password);
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_configuration));
+    vTaskDelay(2000 / portTICK_PERIOD_MS); 
+    ESP_ERROR_CHECK(esp_wifi_start());
+    vTaskDelay(2000 / portTICK_PERIOD_MS); 
+    ESP_ERROR_CHECK(esp_wifi_connect());
+
 }
 
 void WifiDriver::wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id,void *event_data){
@@ -68,18 +87,22 @@ void WifiDriver::wifi_event_handler(void *event_handler_arg, esp_event_base_t ev
         case WIFI_EVENT_STA_DISCONNECTED:
             ESP_LOGI(TAG, "WiFi lost connection");
             
-            if(retry_num<5){
+            if(retry_num< EXAMPLE_ESP_MAXIMUM_RETRY){
                 esp_wifi_connect();
                 retry_num++;
                 ESP_LOGI(TAG, "Retrying to Connect...");
             }
             break;
 
-        case IP_EVENT_STA_GOT_IP:
-            ESP_LOGI(TAG, "Wifi got IP...");
-        break;
+        case IP_EVENT_STA_GOT_IP: {
+                ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+                char ip_str[IP4ADDR_STRLEN_MAX];
+                esp_ip4addr_ntoa(&event->ip_info.ip, ip_str, IP4ADDR_STRLEN_MAX);
+                ESP_LOGI(TAG, "Got IP: %s", ip_str);
+            }
+            break;
         
         default:
-        break;
+            break;
     }
 }
--- main/main.cpp
+++ main/main.cpp
@@ -7,6 +7,10 @@ extern "C" void app_main(void) {
 
     WifiDriver *wifi = new WifiDriver();
     wifi->start();
+
+    while(1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    }
     
     delete wifi;
 }
